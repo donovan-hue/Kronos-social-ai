c5e02dde9d71ace63e703c26aba49cdc21e56498
Legarzee <doonovaanga@gmail.com>
2026-09-01 18:14:53 -0600

    Add aiLimiter middleware to image routes

--- server/src/modules/image-ai/image.routes.js
+++ server/src/modules/image-ai/image.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const auth = require("../../middleware/auth");
+const aiLimiter = require("../../middleware/aiLimiter");
 const { handleUpload } = require("../../middleware/upload");
 const imageService = require("./image.service");
 
